516237647978399ac2d6bbdc0c19d8f1153fc45d
Tim <tim@localhost.localdomain>
2021-03-28 14:42:07 +0100

    changed to use alternative to gh-pages

--- src/Core/API/axiosInstance.js
+++ src/Core/API/axiosInstance.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production' ? process.env.PROD_URI : process.env.DEV_URI;
 
-console.log(baseURL)
-
 const api = axios.create({
     baseURL,
     headers: { 'Access-Control-Allow-Origin': '*' },
